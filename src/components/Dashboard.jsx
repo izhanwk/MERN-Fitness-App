4dcf2f7aed0d54f820f194f534d656fe3d811435
izhanwk <izhanwaseem6@gmail.com>
2025-09-17 19:34:20 +0500

    undo

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -114,6 +114,11 @@ const NutritionTracker = () => {
           validateStatus: () => true,
         });
         const data = res.data;
+        if (res.status < 200 || res.status >= 300) {
+          alert("Token expired");
+          navigate("/signin");
+          return;
+        }
         setuserData((prev) => ({ ...prev, ...data }));
         setmode(data.mode);
         setuserName(data.name || "");
