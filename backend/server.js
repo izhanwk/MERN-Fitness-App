0dcd0f96203365f2a36a3a68ae49ded36cf7651f
izhanwk <izhanwaseem6@gmail.com>
2025-10-05 18:10:07 +0500

    server update 2

--- backend/server.js
+++ backend/server.js
@@ -285,7 +285,8 @@ app.get("/getfood", verifyToken, async (req, res) => {
 });
 
 app.get("/getfood2", verifyToken, async (req, res) => {
-  const page = parseInt(req.query.page) || 1; // default page = 1
+  console.log("Page from fontend : ", req.query.page);
+  const page = parseInt(req.query.page) || 0; // default page = 1
   try {
     console.log("Page no : ", page);
     const totalLength = await Foods.countDocuments({});
