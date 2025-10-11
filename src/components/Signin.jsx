8e675e4fd1e64720cf57cc576986121847f4af34
izhanwk <izhanwaseem6@gmail.com>
2025-10-11 16:00:09 +0500

    opacity

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -405,7 +405,10 @@ function Signin() {
                     </span>
                     <span className="h-px flex-1 bg-white/20"></span>
                   </div>
-                  <div className="flex justify-center" ref={googleButtonRef}>
+                  <div
+                    className="flex justify-center opacity-50"
+                    ref={googleButtonRef}
+                  >
                     <button
                       id="customGoogleBtn"
                       class="flex items-center justify-center gap-3 w-full sm:w-auto bg-white border border-gray-300 text-gray-600 font-medium rounded-md px-5 py-2 hover:bg-gray-50 transition duration-200"
